c2a94c4c56f8ea43e08cd66514729efee7b649d3
dokim <dogdo5726@gmail.com>
2022-10-28 13:41:38 +0900

    [JDBC] making dao

--- src/main/java/com/springboot/hello/dao/AddStrategy.java
+++ src/main/java/com/springboot/hello/dao/AddStrategy.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AddStrategy implements StatementStrategy{
-    User user;
+    User user ;
 
     public AddStrategy(User user) {
         this.user = user;
