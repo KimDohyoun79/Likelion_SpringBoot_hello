504becd7cc2cf5f26d8123a316f60eef8736f4cd
dokim <dogdo5726@gmail.com>
2022-11-01 15:01:34 +0900

    inset가 잘 되는지, 성공

--- src/main/java/com/springboot/hello/Hospital/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/Hospital/dao/HospitalDao.java
@@ -1,6 +1,7 @@
 package com.springboot.hello.Hospital.dao;
 
 import com.springboot.hello.Hospital.domain.Hospital;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -14,20 +15,29 @@ public class HospitalDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+
+    public int getCount() {
+        String sql = "select count(id) from nation_wide_hospitals;";
+        return this.jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+
+
     // list<Hostipal> --11만건
-    public void add(Hospital hospital){
-        String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_data`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_cnt`, `patient_room_count`, `total_number_of_beds`, `total_area_size`) " +
-                "VALUES (?, ?, ?," +
-                "?, ?, ?," +
-                "?, ?, ?," +
-                "?, ?, ?," +
-                "?, ?, ?," +
-                "?);"; // 16개
-        this.jdbcTemplate.update(sql, hospital.getId(),hospital.getOpenServiceName(),hospital.getOpenLocalGovernmentCode(),
+    public void add(Hospital hospital) {
+        String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_data`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_cnt`, `patient_room_count`, `total_number_of_beds`, `total_area_size`)" +
+                " VALUES (?,?,?," +
+                "?,?,?," +
+                "?,?,?," +
+                "?,?,?," +
+                "?,?,?," +
+                "?);";// 16개
+
+        this.jdbcTemplate.update(sql, hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
                 hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(),
                 hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(),
                 hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
                 hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
                 hospital.getTotalAreaSize());
+
     }
 }
